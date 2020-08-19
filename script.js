2568c4519534cdd26b5da538b05315ec6bec36f4
cascutter <cascutter13@gmail.com>
2020-08-19 10:08:10 -0500

    Added scheduling layout using jquery; clock working and updating; handles on all divs

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-// Time blocks
-var myDaySchedule = {
-    "9 AM" : "",
-    "10 AM" : "",
-    "11 AM" : "",
-    "12 PM" : "",
-    "1 PM" : "",
-    "2 PM" : "",
-    "3 PM" : "",
-    "4 PM" : "",
-    "5 PM" : "",
-}
-//When planner is opened, current day shows at top
-$("#currentDay").text(moment().format('LLLL')); 
+$(document).ready(function () {
 
-//
-$("button").click(function() {
-    value = $(this).siblings("textarea").val();
-    hourOfDay = $(this).siblings("div").text();
-    saveInput(hourOfDay, value);
-});
+    // Displays and updates time/date 
+    const calendarTime = function () {
+    $("#currentDay").text(moment().format('LLLL')); 
+    }; 
+    calendarTime()
+    setInterval(calendarTime, 1000);
 
+    // Array of time blocks by hour
+    var timeBlocks = ["9 AM" , "10 AM", "11 AM", "12 PM", "1 PM" , "2 PM" , "3 PM" , "4 PM" , "5 PM"];
 
-//Time blocks for standard business hours
-//Time blocks for that day are color-coded to show, past, present, and future
-//Time blocks can be clicked and events can be entered
-//Events entered are saved to local storage
-//When page refreshes, events persist
+    // Adding in schedule layout and defining elements
+    for (let i = 0; i < timeBlocks.length; i++) {
+        var newDiv = $("<div>");
+        var timeDiv = $("<div>");
+        var scheduleDiv = $("<textarea>");
+        var saveEl = $("<button>");
+        var iconEl = $("<i>");
+
+        // Adding classes and handles for css elements
+        newDiv.attr("id", "time-block", timeBlocks[i]);
+        newDiv.attr("class", "row");
+        timeDiv.attr("class", "hour col-1");
+        scheduleDiv.attr("class", "schedule col-10");
+        saveEl.attr("class", "saveBtn col-1");
+        iconEl.attr("class", "far fa-save");
+
+        // Displaying to window
+        $(".container").append(newDiv);
+        newDiv.append(timeDiv);
+        newDiv.append(scheduleDiv);
+        newDiv.append(saveEl);
+        saveEl.append(iconEl);
+        timeDiv.text(timeBlocks[i])
+    };
+
+    let time = moment();
+    function changeRowColor() {
+     hour = time.hours();
+     $("#schedule").each(function() {
+        let currentHour = parseInt($(this).attr("id"));
+        if (currentHour > hour) {
+            $(this).addClass("future")
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+        });
+    } 
+    changeRowColor ();
+    setInterval(changeRowColor, 1000);
+})
